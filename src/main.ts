71c6d8a047747931e9b293d04c8aa897a72215b9
Frederic Brodbeck <der.froedi@gmail.com>
2024-05-22 14:26:07 +0200

    use logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const port = process.env.PORT;
-  console.log(`http://localhost:${port}`);
+
+  const logger = new Logger('bootstrap');
+  logger.log(`http://localhost:${port}`);
 
   await app.listen(port);
 }
