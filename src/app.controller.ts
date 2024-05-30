c5cb11e99ee003388c2bfbef50a24806ef90c611
Frederic Brodbeck <der.froedi@gmail.com>
2024-05-30 10:35:41 +0200

    removed unused code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { AppService } from './app.service';
-import { EmailService } from './email/email.service';
-import EmailTestDto from './dtos/EmailTestDto';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly emailService: EmailService,
-  ) {}
-
-  // TODO: remove
-  @Post('test/email')
-  testEmail(@Body() emailTestDto: EmailTestDto): string {
-    try {
-      this.emailService.sendEmail(
-        emailTestDto.to,
-        emailTestDto.subject || 'subject',
-        emailTestDto.text || 'content',
-      );
-      return 'success';
-    } catch (err) {
-      return err.toString();
-    }
-  }
-}
+export class AppController {}
