de4d5a33aa93f2dea340c05fe4ba47b37fceaa1e
Frederic Brodbeck <der.froedi@gmail.com>
2024-05-30 10:56:00 +0200

    removed unused code

--- src/matrix/matrix.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { MatrixService } from './matrix.service';
-
-@Controller('matrix')
-export class MatrixController {
-  constructor(private readonly matrixService: MatrixService) {}
-}
